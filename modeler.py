65aab2cd10f1fd693bb01a0f9cef06dd5623b251
Raphael Freudiger <freudrap@students.zhaw.ch>
2018-02-27 13:50:18 +0100

    merge leaky and dropout models
    Setting all parameters to 0 results in original acapellabot models.
    For other values any combination of leaky relu and dropout can be used.

--- modeler.py
+++ modeler.py
@@ -51,101 +51,70 @@ class Modeler(object):
         acapella = conv
         return Model(inputs=mashup, outputs=acapella)
 
-    def leaky_acapellabot(self, alpha1, alpha2):
+    def leaky_dropout(self, alpha1, alpha2, rate):
         mashup = Input(shape=(None, None, 1), name='input')
-        convA = Conv2D(64, 3, padding='same')(mashup)
+        dropout = Dropout(rate)(mashup)
+
+        convA = Conv2D(64, 3, padding='same')(dropout)
         convA = LeakyReLU(alpha=alpha1)(convA)
+        convA = Dropout(rate)(convA)
+
         conv = Conv2D(64, 4, strides=2, padding='same', use_bias=False)(convA)
         conv = LeakyReLU(alpha=alpha1)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = BatchNormalization()(conv)
 
         convB = Conv2D(64, 3, padding='same')(conv)
         convB = LeakyReLU(alpha=alpha1)(convB)
+        convB = Dropout(rate)(convB)
+
         conv = Conv2D(64, 4, strides=2, padding='same', use_bias=False)(convB)
         conv = LeakyReLU(alpha=alpha1)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = BatchNormalization()(conv)
 
         conv = Conv2D(128, 3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = Conv2D(128, 3, padding='same', use_bias=False)(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = BatchNormalization()(conv)
         conv = UpSampling2D((2, 2))(conv)
 
         conv = Concatenate()([conv, convB])
+
         conv = Conv2D(64, 3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = Conv2D(64, 3, padding='same', use_bias=False)(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = BatchNormalization()(conv)
         conv = UpSampling2D((2, 2))(conv)
 
         conv = Concatenate()([conv, convA])
+
         conv = Conv2D(64, 3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = Conv2D(64, 3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = Conv2D(32, 3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
+        conv = Dropout(rate)(conv)
+
         conv = Conv2D(1, 3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
+
         acapella = conv
-        return Model(inputs=mashup, outputs=acapella)
-
-    def dropout_acapellabot(self, rate):
-        mashup = Input(shape=(None, None, 1), name='input')
-        dropout = Dropout(rate)(mashup)
-
-        convA = Conv2D(64, 3, activation='relu', padding='same')(dropout)
-        dropout = Dropout(rate)(convA)
-
-        conv = Conv2D(64, 4, strides=2, activation='relu',
-                      padding='same', use_bias=False)(dropout)
-        dropout = Dropout(rate)(conv)
-
-        conv = BatchNormalization()(dropout)
-
-        convB = Conv2D(64, 3, activation='relu', padding='same')(conv)
-        dropout = Dropout(rate)(convB)
-
-        conv = Conv2D(64, 4, strides=2, activation='relu',
-                      padding='same', use_bias=False)(dropout)
-        dropout = Dropout(rate)(conv)
-
-        conv = BatchNormalization()(dropout)
-
-        conv = Conv2D(128, 3, activation='relu', padding='same')(conv)
-        dropout = Dropout(rate)(conv)
-
-        conv = Conv2D(128, 3, activation='relu',
-                      padding='same', use_bias=False)(dropout)
-        dropout = Dropout(rate)(conv)
-
-        conv = BatchNormalization()(dropout)
-        conv = UpSampling2D((2, 2))(conv)
-
-        conv = Concatenate()([conv, convB])
-        conv = Conv2D(64, 3, activation='relu', padding='same')(conv)
-        dropout = Dropout(rate)(conv)
-
-        conv = Conv2D(64, 3, activation='relu',
-                      padding='same', use_bias=False)(dropout)
-        dropout = Dropout(rate)(conv)
-
-        conv = BatchNormalization()(dropout)
-        conv = UpSampling2D((2, 2))(conv)
-
-        conv = Concatenate()([conv, convA])
-        conv = Conv2D(64, 3, activation='relu', padding='same')(conv)
-        dropout = Dropout(rate)(conv)
-
-        conv = Conv2D(64, 3, activation='relu', padding='same')(dropout)
-        dropout = Dropout(rate)(conv)
-
-        conv = Conv2D(32, 3, activation='relu', padding='same')(dropout)
-        dropout = Dropout(rate)(conv)
-
-        conv = Conv2D(1, 3, activation='relu', padding='same')(dropout)
-        acapella = conv
-
         return Model(inputs=mashup, outputs=acapella)
